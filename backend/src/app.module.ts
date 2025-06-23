e0b977c0219a62251e80730ecf940aeb1a5817d0
MACBOOK PRO 2019 <thanglphce171637@fpt.edu.vn>
2025-06-24 05:20:42 +0700

    no comment

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,16 +1,21 @@
-// src/app.module.ts
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config'; // <-- thêm dòng này
+import { ConfigModule } from '@nestjs/config';
 import { CVModule } from './cv/cv.module';
 import { ContactModule } from './contact/contact.module';
 
 @Module({
   imports: [
+    // Load .env và cho phép dùng biến môi trường ở mọi nơi
     ConfigModule.forRoot({
-      isGlobal: true, // Giúp toàn bộ app dùng được process.env
+      isGlobal: true,
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/cvdb'),
+    
+    // Kết nối MongoDB qua biến môi trường
+   MongooseModule.forRoot(process.env.MONGODB_URI as string),
+
+    
+    // Các module của bạn
     CVModule,
     ContactModule,
   ],
